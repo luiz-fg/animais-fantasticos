a9fc029abd9a734512e16676d4f653effd77dc65
luiz-fg <luiz.gomes@universo.univates.br>
2024-08-30 19:17:59 -0300

    arrumado erro de caminho animaiapi.json

--- js/script.js
+++ js/script.js
@@ -45,5 +45,5 @@ menuMobile.init();
 
 initFuncionamento();
 
-fetchAnimais("../animaisapi.json", ".numeros-grid");
+fetchAnimais("./animaisapi.json", ".numeros-grid");
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
